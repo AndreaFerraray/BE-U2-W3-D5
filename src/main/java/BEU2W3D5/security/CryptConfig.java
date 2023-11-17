d8e18fc1f1b246fed34493842afb12f62e3b0a97
AndreaFerrara13 <138196137+AndreaFerraray@users.noreply.github.com>
2023-11-17 16:06:33 +0100

    cryptoConfig

---
+++ src/main/java/BEU2W3D5/security/CryptConfig.java
@@ -0,0 +1,13 @@
+package BEU2W3D5.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class CryptConfig {
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder(11);
+    }}
